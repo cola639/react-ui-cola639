c93df8c043aa9c03e69bb8f0e419aa27009a99ac
cola639 <337507950@qq.com>
2023-02-09 01:51:47 +0800

    add

---
+++ src/components/upload/__stories__/Wrap.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Wrap } from '../index';
+
+export default {
+	title: 'Components/Upload',
+	component: Wrap,
+} as ComponentMeta<typeof Wrap>;
+
+const Template: ComponentStory<typeof Wrap> = (args) => <Wrap {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	children: (
+		<p style={{ background: 'beige', padding: '20px' }}>
+			max-width default is 1024px.
+		</p>
+	),
+};
